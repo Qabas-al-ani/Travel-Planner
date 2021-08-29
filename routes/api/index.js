3a1c820b6a6df7bc5825ea1b63cad04e3dc6a64c
Qabas al ani <doctorqa@yahoo.com>
2021-08-29 16:19:12 -0400

    created routes index.js

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const travelersRoutes = require("./travelersRoutes");
+const locationsRoutes = require("./locationsRoutes");
+const tripsRoutes = require("./tripsRoutes");
+
+router.use("/travelers", travelersRoutes);
+router.use("/locations", locationsRoutes);
+router.use("/trips", tripsRoutes);
+
+module.exports = router;
